0c0b6481bf771536daf3b11d3bb7b667047c767b
bweidenbaum <bweidenbaum@bweidenbaum-macbookair.roam.corp.google.com>
2014-08-06 22:11:25 -0700

    make all - and + buttons update the graph using ngrepeat

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -6,7 +6,13 @@ angular.module('zoosk')
     cows: 8
   };
 
+  $scope.animals = Object.keys($scope.counts);
+
   $scope.incrementAnimal = function(animal) {
     $scope.counts[animal]++;
+  };
+
+  $scope.decrementAnimal = function(animal) {
+    $scope.counts[animal]--;
   };
 });
